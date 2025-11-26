7521f20b817a766f7f176d3f7918178d80bbb014
Gayatri-Lagudu <gayatrilagudu1524@gmail.com>
2025-11-26 10:36:16 +0530

    c prac

---
+++ Global & Local Variables.c
@@ -0,0 +1,20 @@
+//program on global and local variables
+#include<stdio.h>
+int x=10;
+void display();
+main()
+{
+	int y=20;
+	printf("the value of x is %d\n",x);
+	printf("the value of y is %d\n",y);
+	//printf("the value of z is %d\n")
+	display();
+}
+void display()
+{
+	int z=30;
+	printf("the value of x is %d\n",x);
+	//printf("the value of y is %d\n",y)
+	printf("the value of z is %d\n",z);
+}
+
